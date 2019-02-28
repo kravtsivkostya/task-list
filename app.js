4f7bcd5617266cc693258c9d909822f2f5560a1d
kostyakravtsiv <kostya.kravtsiv@gmail.com>
2019-02-28 17:36:32 +0200

    start

---
+++ app.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('#task-form');
+const clearBtn = document.querySelector('.clear-tasks btn black');
+const taskList = document.querySelector('.collection');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
