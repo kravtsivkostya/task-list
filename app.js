54ffab9e4199c7105b6e9bade531a511fccb640b
kostyakravtsiv <kostya.kravtsiv@gmail.com>
2019-02-28 22:55:04 +0200

    ui

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('#task-form');
-const clearBtn = document.querySelector('.clear-tasks btn black');
+const clearBtn = document.querySelector('.clear-tasks ');
 const taskList = document.querySelector('.collection');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
@@ -9,10 +9,58 @@ loadEventListeners();
 function loadEventListeners(){
 
     form.addEventListener('submit', addTask);
+    taskList.addEventListener('click', removeTask);
+    clearBtn.addEventListener('click',removeAllTasks);
+    filter.addEventListener('keyup',filterTasks);
 }
 
 function addTask(e){
     if(taskInput.value === ''){
         alert('Add a task');
     }
+
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-times"> </i>';
+    li.appendChild(link);
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+
+    e.preventDefault();
+}
+function removeTask(e){
+   
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Delete Task?')){
+            e.target.parentElement.parentElement.remove();
+        }
+
+    }
+}
+function removeAllTasks(e){
+    if(confirm('Are you sure?')){
+       while(taskList.firstChild){
+           taskList.removeChild(taskList.firstChild);
+       }
+    }
+    
+}
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach
+    (function(task){
+            const item = task.firstChild.textContent;
+            if(item.toLowerCase().indexOf(text) != -1){
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+            };
+        }
+    )
+
 }
