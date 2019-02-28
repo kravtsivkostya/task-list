09d6be6b2d34ee433074e873ea075de1adeed810
kostyakravtsiv <kostya.kravtsiv@gmail.com>
2019-02-28 17:47:23 +0200

    test

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ const clearBtn = document.querySelector('.clear-tasks btn black');
 const taskList = document.querySelector('.collection');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+loadEventListeners();
+
+function loadEventListeners(){
+
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Add a task');
+    }
+}
